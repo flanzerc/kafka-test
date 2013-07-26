4f99256021bee65cdfcc0c37b0b4da3839c95ba2
flanzerc <amardep.sarkar@gmail.com>
2013-07-26 19:16:01 +0530

    added consumers

---
+++ src/main/java/com/consumers/SimpleConsumerWithPartition.java
@@ -0,0 +1,44 @@
+package com.consumers;
+
+import kafka.api.FetchRequest;
+import kafka.javaapi.consumer.SimpleConsumer;
+import kafka.javaapi.message.ByteBufferMessageSet;
+import kafka.message.MessageAndOffset;
+import kafka.utils.Utils;
+
+public class SimpleConsumerWithPartition {
+
+	private static void prepareConsumer(SimpleConsumer consumer, String topicName, int partition) {
+		long offset = 0;
+		while (true) {
+			FetchRequest fetchrequest = new FetchRequest(topicName, 0, offset, 1000000);
+			ByteBufferMessageSet messages = consumer.fetch(fetchrequest);
+			for (MessageAndOffset msg : messages) {
+				System.out.println("consumed: " + Utils.toString(msg.message().payload(), "UTF-8"));
+				offset = msg.offset();
+			}
+
+		}
+	}
+
+	public static void main(String[] args) {
+
+		SimpleConsumer consumer = new SimpleConsumer("127.0.0.1", 9092, 10000, 1024000);
+		// long offset = 0;
+		// while (true) {
+		// FetchRequest fetchrequest = new FetchRequest("test-topic-message1",
+		// 0, offset, 1000000);
+		// ByteBufferMessageSet messages = consumer.fetch(fetchrequest);
+		// for (MessageAndOffset msg : messages) {
+		// System.out.println("consumed: " +
+		// Utils.toString(msg.message().payload(), "UTF-8"));
+		// offset = msg.offset();
+		// }
+		//
+		// }
+
+		SimpleConsumerWithPartition.prepareConsumer(consumer, "test-topic-message1", 1);
+
+	}
+
+}
